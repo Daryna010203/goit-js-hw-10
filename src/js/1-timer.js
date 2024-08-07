ac9d626579dffc56d4e105258e215aac4ac65037
Daryna Kurytsyna <darina.kuritsina@gmail.com>
2024-08-08 01:26:05 +0200

    add calendar

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -15,3 +15,16 @@ const options = {
 
 const Btn = document.querySelector('button');
 Btn.classList.add('js-button');
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  position: 'auto center',
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+});
+
+console.dir(flatpickr);
