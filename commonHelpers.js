a974effde13d6ca28af83316e8a5f108097b6d5c
Daryna010203 <152505147+Daryna010203@users.noreply.github.com>
2024-08-07 22:30:38 +0000

    Deploying to gh-pages from @ Daryna010203/goit-js-hw-10@3191f0f14ba204bf9a0b32bf489437f36e963793 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/vendor-94c342ae.js";const t=document.querySelector("button");t.classList.add("js-button");
+//# sourceMappingURL=commonHelpers.js.map
